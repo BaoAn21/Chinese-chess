9f93dee284f0c63cee9c84b3f29d46a796ee6c25
AnTran <cao.an99@gmail.com>
2024-07-18 14:09:01 +0700

    Initial Commit

---
+++ ChineseChess/ChineseChessApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChineseChessApp.swift
+//  ChineseChess
+//
+//  Created by Trần Ân on 18/7/24.
+//
+
+import SwiftUI
+
+@main
+struct ChineseChessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChineseChess/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ChineseChess
+//
+//  Created by Trần Ân on 18/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
